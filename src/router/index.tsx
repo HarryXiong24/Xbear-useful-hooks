987cf01666a50f13c401bf36c19c9c2fc207d58d
harryxiong <harryxiong24@gmail.com>
2021-12-09 11:48:52 +0800

    init

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import { RouteObject } from 'react-router';
 import Home from '@/pages/Home/index';
-import Demo from '@/pages/Demo/index';
 import NoMatch from '@/pages/404/index';
-import React from 'react';
+// import Demo from '@/pages/Demo/index';
+
+const Demo = React.lazy(() => import('@/pages/Demo/index'));
 
 const routes: RouteObject[] = [
   { path: '/', element: <Home />, index: true },
